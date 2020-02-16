d70bd0277324a6af1129c0dde25166cac17273e0
Sebastian Morgado <smorgado@uade.edu.ar>
2020-02-16 17:41:01 -0300

    Updates

---
+++ precache-manifest.5409491fadf0bff38d3028eac78b03b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcb72f9dc57d5992b9697939b495f40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2aa4c45021b8f5af4000",
+    "url": "/monsters-rolodex/static/css/main.71948470.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa4c45021b8f5af4000",
+    "url": "/monsters-rolodex/static/js/main.dc38d9d7.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
